d403b8a1a884694f407dd65833bc210d5f17ba06
Peter <christianpb25@gmail.com>
2024-03-14 16:09:42 +0100

    Added logo and comments

--- html/app.js
+++ html/app.js
@@ -1,10 +1,10 @@
 window.addEventListener('message', function (event) {
   switch (event.data.message) {
     case 'togglestore':
-      invokeNative('openUrl', 'https://srpeter.tebex.io/');
+      invokeNative('openUrl', 'https://srpeter.tebex.io/'); // Store URL
       break;
     case 'togglediscord':
-      invokeNative('openUrl', 'https://discord.gg/6XN5C9J6BT');
+      invokeNative('openUrl', 'https://discord.gg/6XN5C9J6BT'); // Discord URL
       break;
     default:
       console.log('Error');
